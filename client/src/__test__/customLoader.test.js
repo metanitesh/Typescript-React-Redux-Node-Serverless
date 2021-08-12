4e9ff144378d36fc8369ad6419a5caab4d024138
Nitesh Sharma <nitesh.grep@gmail.com>
2021-08-12 12:46:47 +0530

    Adding boilerplate code

--- client/src/__test__/customLoader.test.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@jest/globals";
-import { render } from "@testing-library/react";
-import { renderHook } from "@testing-library/react-hooks";
-import useLoader from "../components/useLoader";
-
-test("Default loader set to be false", async () => {
-  const [loader, setLoader] = renderHook(useLoader).result.current
-  expect(loader).toBe(false)
-});
--- server/config.js
+++
@@ -1,9 +0,0 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-    user:'postgres', // default postgres
-    host:'localhost',
-    database:'courses-app-db', // `my_todos_db`
-    port:'5432' //default port
-});
-
-module.exports = pool;
